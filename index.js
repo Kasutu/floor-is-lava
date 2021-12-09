84312f8a01cbee5ff588a77b404024883b21874a
PZYCO20 <64959978+PZYCO20@users.noreply.github.com>
2021-12-09 12:47:31 +0800

    added more info in comments

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 // NOTES
 // lets use the standard loop of airbnb
 // for (let i = 0; i < range; i += 1) {}
+//
+// steps in committing in git and updating the code
+// git add .
+// git commit -m "Description"
+// git push origin main
+// 
+// more git tutorials here
+//https://docs.github.com/en/get-started/using-git/pushing-commits-to-a-remote-repository
+//
 // You can add notes here on the top page.
 
 //begin
