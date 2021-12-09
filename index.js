482c69ac3a6a014ac8d1b2d1297394f5c1573300
PZYCO20 <64959978+PZYCO20@users.noreply.github.com>
2021-12-09 12:40:26 +0800

    Prepared the core files

---
+++ index.js
@@ -0,0 +1,11 @@
+// NOTES
+// lets use the standard loop of airbnb
+// for (let i = 0; i < range; i += 1) {}
+// You can add notes here on the top page.
+
+//begin
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "green";
+ctx.fillRect(10, 10, 100, 100);
