2701dbe4ac18a906bbe90b03cd7ff141c698fbc8
PZYCO20 <64959978+PZYCO20@users.noreply.github.com>
2021-12-10 21:24:01 +0800

    added a test rectangle

--- index.js
+++ index.js
@@ -18,19 +18,24 @@
 //Begin
 let canvas;
 let ctx;
-let currentTime;
+let currentTime = Date.now();
+let animationFrame;
 let secondsPassed = 0;
 let lastFrame = 0;
 
-//starts the game
+window.onload = pageLoad();
+
+//fires the loop
 function pageLoad() {
   canvas = document.querySelector('canvas');
   ctx = canvas.getContext('2d');
-  currentTime = Date.now();
 
   // sets the canvas width and hight dynamically
   canvas.width = innerWidth;
   canvas.height = innerHeight;
+
+  //first animation request
+  animationFrame = requestAnimationFrame(gameLoop);
 }
 
 function gameLoop(currentTime) {
@@ -39,8 +44,22 @@ function gameLoop(currentTime) {
   lastFrame = currentTime;
 
   // Pass the delta time to the update
-  update(delta);
-  draw();
+  // Game logic here
+  // update(delta);
+  render();
 
   requestAnimationFrame(gameLoop);
+}
+
+function render() {
+  let randomColor = Math.random() > 0.5 ? '#ff8080' : '#0099b0';
+  let x = 0;
+  let y = 0;
+
+  // Clear the entire canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  //draw a rectangle
+  ctx.fillStyle = randomColor;
+  ctx.fillRect(x, y, 200, 200);
 }
