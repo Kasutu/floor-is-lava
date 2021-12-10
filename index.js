b3d8ec2b506e5dc27c35c0545ee04fb47979121b
PZYCO20 <64959978+PZYCO20@users.noreply.github.com>
2021-12-11 00:09:20 +0800

    added collision border and class properties

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ function pageLoad() {
   ctx = canvas.getContext('2d');
 
   // sets the canvas width and hight dynamically
-  canvas.width = innerWidth;
-  canvas.height = innerHeight;
+  canvas.width = 1280;
+  canvas.height = 720;
 
   //first animation request
   animationFrame = requestAnimationFrame(gameLoop);
@@ -46,20 +46,74 @@ function gameLoop(currentTime) {
   // Pass the delta time to the update
   // Game logic here
   // update(delta);
-  render();
+  // render();
 
   requestAnimationFrame(gameLoop);
 }
 
-function render() {
-  let randomColor = Math.random() > 0.5 ? '#ff8080' : '#0099b0';
-  let x = 0;
-  let y = 0;
+//Classes
+class Player {
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+  }
 
-  // Clear the entire canvas
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  //draw a rectangle
-  ctx.fillStyle = randomColor;
-  ctx.fillRect(x, y, 200, 200);
+  render() {
+    //player stroke rectangle
+    ctx.strokeRect(this.x, this.y, this.width, this.height);
+  }
 }
+
+class Platform {
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+  }
+
+  render() {
+    //stroke rectangle
+    ctx.strokeRect(this.x, this.y, this.width, this.height);
+  }
+}
+
+class Lava {
+  constructor(y) {
+    this.y = y;
+  }
+
+  render() {
+    //stroke rectangle
+    ctx.strokeRect(0, this.y, 1280, 50);
+  }
+}
+
+const x = canvas.width / 2;
+const y = canvas.height / 2;
+
+//entity settings
+//1280 x 720 resolution
+const player = new Player(x - 50, y - 100, 100, 200);
+const platform = new Platform(x - 100, y + 100, 200, 20);
+const lava = new Lava(720 - 80);
+
+//draws the shapes
+player.render();
+platform.render();
+lava.render();
+
+// function render() {
+//   let randomColor = Math.random() > 0.5 ? '#ff8080' : '#0099b0';
+//   const x = canvas.width / 2;
+//   const y = canvas.height / 2;
+
+//   // Clear the entire canvas
+//   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+//   //draw a rectangle
+//   ctx.fillStyle = randomColor;
+//   ctx.fillRect(x - 100, y - 100, 200, 200);
+// }
