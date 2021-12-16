b3093d8d9be31043c20611e4fb3148844013c196
PZYCO YT <64959978+PZYCO20@users.noreply.github.com>
2021-12-16 13:00:30 +0800

    better platform detection and colision
    - code is kinda messy i will refactor it later

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ let lastFrame = 0;
 let fps;
 let gameStart = true;
 
-// movement direction
-let direction = {
+// movement joystick
+let joystick = {
   up: false,
   down: false,
   left: false,
@@ -21,17 +21,16 @@ let direction = {
   engagedKey: '',
   inputListener: function (event) {
     let inputState = event.type === 'keydown' ? true : false;
-    this.engagedKey = event.type;
 
     switch (event.key) {
       case 'ArrowUp': // up arrow key
-        direction.up = inputState;
+        joystick.up = inputState;
         break;
       case 'ArrowLeft': // left arrow key
-        direction.left = inputState;
+        joystick.left = inputState;
         break;
       case 'ArrowRight': // right arrow key
-        direction.right = inputState;
+        joystick.right = inputState;
         break;
     }
   },
@@ -42,23 +41,26 @@ let playerSize = 16;
 let player = {
   width: playerSize,
   height: playerSize,
-  x: Math.floor(width / 2 - playerSize / 2),
-  y: Math.floor(height / 2 - playerSize / 2),
+  x: Math.trunc(width / 2 - playerSize / 2),
+  y: Math.trunc(height / 2 - playerSize / 2),
+  oldX: 0,
+  oldY: 0,
   velX: 0,
   velY: 0,
   isJumping: false,
   isOnFloor: false,
   isUnder: false,
   isOutSide: false,
-  isInside: false,
+  isOnPlatform: false,
+  isAirborne: false,
 
   draw: function () {
     // draw a stroke rectangle
     ctx.strokeStyle = 'red';
     ctx.lineWidth = 2;
     ctx.strokeRect(
-      Math.floor(this.x),
-      Math.floor(this.y),
+      Math.trunc(this.x),
+      Math.trunc(this.y),
       this.width,
       this.height
     );
@@ -66,8 +68,10 @@ let player = {
 };
 
 let platform = {
-  x: Math.floor(width / 2 - 150 / 2),
-  y: Math.floor(height - 90),
+  x: Math.trunc(width / 2 - 150 / 2),
+  y: Math.trunc(height - 90),
+  oldX: 0,
+  oldY: 0,
   width: 150,
   height: 15,
 
@@ -76,8 +80,8 @@ let platform = {
     ctx.strokeStyle = 'black';
     ctx.lineWidth = 1;
     ctx.strokeRect(
-      Math.floor(this.x),
-      Math.floor(this.y),
+      Math.trunc(this.x),
+      Math.trunc(this.y),
       this.width,
       this.height
     );
@@ -134,41 +138,69 @@ function gameLoop(currentTime) {
 function update() {
   // physics constants
   const friction = 0.85;
-  const gravity = 0.9;
+  const gravity = 0.7;
   const jumpHeight = 3;
   const movementSpeed = 5;
 
-  // movement and position calculation
-  if (direction.up && !player.isJumping) {
+  // movement and position behavior calculation
+  if (joystick.up && !player.isJumping) {
     player.isJumping = true;
     player.velY = -movementSpeed * jumpHeight;
-    console.log('engagedKey', direction.engagedKey);
+    joystick.up = false;
   }
 
-  if (direction.left) {
-    player.velX -= 1;
+  if (joystick.left) {
+    player.velX -= 1.5;
+
+    // left side world collision
+    if (player.x <= width - width) {
+      player.velX = 0;
+    }
   }
 
-  if (direction.right) {
-    player.velX += 1;
+  if (joystick.right) {
+    player.velX += 1.5;
+
+    // right side world collision
+    if (player.x + player.width >= width) {
+      player.velX = 0;
+    }
   }
 
   // friction
-  player.velX *= friction;
+  if (!player.isJumping) {
+    player.velX *= friction;
+  } else {
+    player.velX *= 0.85;
+  }
   player.velY += gravity;
-  // console.log('step 2:', player.velY)
 
   // moves the player
   // final position values
+  player.oldX = player.x;
+  player.oldY = player.y;
   player.y += player.velY;
   player.x += player.velX;
-  // console.log('step 3:', player.y)
+
+  // platform
+  platform.oldY = platform.y;
+
+  // world bottom border collision
+  if (
+    player.y + player.height >= height &&
+    player.y >= height - player.height
+  ) {
+    player.y = height - player.width;
+    player.velY = 0;
+    player.isOnFloor = true;
+    player.isJumping = false;
+  }
 
   // checks if player is under platform
   if (
-    player.x > platform.x &&
-    player.x < platform.x + platform.width &&
-    player.y > platform.y
+    player.x >= platform.x &&
+    player.x <= platform.x + platform.width &&
+    player.y >= platform.y
   ) {
     player.isUnder = true;
   } else {
@@ -177,62 +209,37 @@ function update() {
 
   // cliff detection (outside) platform
   if (
-    player.x + player.width < platform.x ||
-    player.x > platform.x + platform.width
+    player.x + player.width <= platform.x ||
+    (player.x >= platform.x + platform.width && !player.isUnder)
   ) {
-    player.isJumping = true;
+    player.isOnPlatform = false;
+    player.isAirborne = true;
     player.isOutSide = true;
     player.isOnFloor = false;
-  }
-
-  // cliff detection (inside) platform
-  if (
-    player.x > platform.x &&
-    player.x - player.width < platform.x + platform.width
-  ) {
-    player.isInside = true;
+  } else if (!player.isUnder) {
+    // cliff detection (inside) platform
+    player.isOnPlatform = true;
+    player.isOnFloor = true;
+    player.isOutSide = false;
+  } else if (player.isUnder) {
     player.isOutSide = false;
   }
 
-  // world bottom border collision
-  if (player.y + player.height > height && player.y > height - player.height) {
-    player.y = height - player.width;
-    player.velY = 0;
-    player.isOnFloor = true;
-    player.isJumping = false;
-  }
-
-  // go through walls if player is under platform
-  if (
-    player.isUnder &&
-    player.isJumping &&
-    player.isInside &&
-    player.y + player.height > platform.y - platform.height / 2
-  ) {
-    player.isUnder = false;
-    player.isJumping = false;
-  }
-
-  // platform collision checks
-  if (
-    platformCollision(player, platform) &&
-    !player.isJumping &&
-    !player.isUnder
-  ) {
-    player.y = platform.y - player.height;
-    player.velY = 0;
-    player.isOnFloor = true;
-    player.isUnder = false;
-  }
-
-  // left side world collision
-  if (player.x <= width - width) {
-    player.x = 0;
-  }
-
-  // right side world collision
-  if (player.x + player.width > width) {
-    player.x = width - player.width;
+  // platform detection
+  if (!player.isOutSide) {
+    if (
+      player.y + player.height > platform.y &&
+      player.oldY + player.height <= platform.oldY
+    ) {
+      player.isJumping = false;
+      // coordinates that make the player stand on platform
+      player.y = platform.y - player.height;
+      player.velY = 0;
+      player.isOnFloor = true;
+      player.isUnder = false;
+      player.isOnPlatform = true;
+      player.isOutSide = false;
+    }
   }
 }
 
@@ -255,13 +262,13 @@ function render() {
   ctx.fillStyle = 'black';
   ctx.fillText(`FPS: ${fps}`, 10, 20);
   ctx.fillText(`Elapsed: ${elapsedTime}s`, 10, 40);
-  ctx.fillText(`player delta-V: ${Math.floor(player.velY)}m/s`, 10, 60);
+  ctx.fillText(`player delta-V: ${Math.trunc(player.velY)}m/s`, 10, 60);
 
   // floating data
   ctx.font = '15px monospace';
   ctx.fillStyle = 'red';
   ctx.fillText(
-    `player: x: ${Math.floor(player.x)} y: ${Math.floor(player.y)}`,
+    `player: x: ${Math.trunc(player.x)} y: ${Math.trunc(player.y)}`,
     player.x - player.width + 50,
     player.y - 30
   );
@@ -286,7 +293,7 @@ function render() {
     player.y - 110
   );
   ctx.fillText(
-    `isInside: ${player.isInside}`,
+    `isOnPlatform: ${player.isOnPlatform}`,
     player.x - player.width + 50,
     player.y - 130
   );
@@ -302,10 +309,10 @@ function render() {
 }
 
 // keypress listener
-addEventListener('keydown', direction.inputListener);
-addEventListener('keyup', direction.inputListener);
+addEventListener('keydown', joystick.inputListener);
+addEventListener('keyup', joystick.inputListener);
 
-// collision detection
+// collision detection box y-axis
 function platformCollision(obj1, obj2) {
   if (obj1.y + obj1.height >= obj2.y && obj1.y <= obj2.y + obj2.height) {
     return true;
@@ -317,9 +324,9 @@ function platformCollision(obj1, obj2) {
 // shows distance visually between 2 objects
 function addLineTrack(target1, target2) {
   let startXLeft = target1.x + target1.width / 2;
-  let startYLeft = target1.y + target1.height / 2;
+  let startYLeft = target1.oldY + target1.height;
   let endXLeft = target2.x + target2.width / 2;
-  let endYLeft = target2.y + target2.height / 2;
+  let endYLeft = target2.oldY;
 
   ctx.beginPath();
   ctx.moveTo(startXLeft, startYLeft); // start coord
@@ -332,28 +339,28 @@ function addLineTrack(target1, target2) {
 // shows boundary visually between 2 objects
 function addLineBoundaries(target) {
   // left side
-  let startXLeft = Math.floor(target.x);
+  let startXLeft = Math.trunc(target.x);
   let startYLeft = 0;
-  let endXLeft = Math.floor(target.x);
-  let endYLeft = Math.floor(height);
+  let endXLeft = Math.trunc(target.x);
+  let endYLeft = Math.trunc(height);
 
   // right side
-  let startXRight = Math.floor(target.x + target.width);
+  let startXRight = Math.trunc(target.x + target.width);
   let startYRight = 0;
-  let endXRight = Math.floor(target.x + target.width);
-  let endYRight = Math.floor(height);
+  let endXRight = Math.trunc(target.x + target.width);
+  let endYRight = Math.trunc(height);
 
   // top side
   let startXTop = 0;
-  let startYTop = Math.floor(target.y);
-  let endXTop = Math.floor(width);
-  let endYTop = Math.floor(target.y);
+  let startYTop = Math.trunc(target.y);
+  let endXTop = Math.trunc(width);
+  let endYTop = Math.trunc(target.y);
 
   // bottom side
   let startXBottom = 0;
-  let startYBottom = Math.floor(target.y + target.height);
+  let startYBottom = Math.trunc(target.y + target.height);
   let endXBottom = width;
-  let endYBottom = Math.floor(target.y + target.height);
+  let endYBottom = Math.trunc(target.y + target.height);
 
   // left side
   ctx.beginPath();
